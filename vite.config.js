c4c9caf21c3ef7207e0aab072c4dafb40d0910e3
Arkadiusz Pietras <62517197+pietrasarkadiusz@users.noreply.github.com>
2024-02-26 18:31:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tip-calculator-app',
+  base: '/tip-calculator-app/',
   plugins: [vue()],
 })
